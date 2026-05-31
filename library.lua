bd8b66b1d6b42c32d3ba0cc8dfa5fb4c4862d0fb
joaolisi130-star <joaolisi130@gmail.com>
2026-05-31 19:35:05 -0300

    Update library.lua

--- library.lua
+++ library.lua
@@ -12,13 +12,13 @@ local MyLibrary = {
 	Themes = {
 		Main = {
 			["Color Hub 1"] = ColorSequence.new({
-			ColorSequenceKeypoint.new(0.00, Color3.fromRGB(25, 25, 25)),
-			ColorSequenceKeypoint.new(0.50, Color3.fromRGB(32.5, 32.5, 32.5)),
-			ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))
-        }),
-			["Color Hub 2"] = Color3.fromRGB(30, 30, 30),
-			["Color Stroke"] = Color3.fromRGB(40, 40, 40),
-			["Color Theme"] = Color3.fromRGB(255, 255, 255),
+				ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 0, 0)),
+				ColorSequenceKeypoint.new(0.50, Color3.fromRGB(25, 0, 0)),
+				ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 0, 0))
+			}),
+			["Color Hub 2"] = Color3.fromRGB(15, 15, 15),
+			["Color Stroke"] = Color3.fromRGB(255, 0, 0),
+			["Color Theme"] = Color3.fromRGB(255, 0, 0),
 			["Color Text"] = Color3.fromRGB(243, 243, 243),
 			["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
 		},
